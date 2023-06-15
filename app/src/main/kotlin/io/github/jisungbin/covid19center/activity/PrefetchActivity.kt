e6312ac09a5f9f282434ca9526b1b87bf055c092
jisungbin <ji@sungb.in>
2023-06-16 00:48:20 +0900

    PrefetchActivity 완성

--- app/src/main/kotlin/io/github/jisungbin/covid19center/activity/PrefetchActivity.kt
+++ app/src/main/kotlin/io/github/jisungbin/covid19center/activity/PrefetchActivity.kt
@@ -7,10 +7,24 @@
 
 package io.github.jisungbin.covid19center.activity
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.jisungbin.covid19center.datasource.dataStore
 import io.github.jisungbin.covid19center.datasource.removeCovidCenterData
@@ -18,10 +32,16 @@ import io.github.jisungbin.covid19center.datasource.writeCovidCenterData
 import io.github.jisungbin.covid19center.model.domain.CovidCenterItem
 import io.github.jisungbin.covid19center.viewmodel.CovidCenterViewModel
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlinx.coroutines.CancellableContinuation
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withTimeout
+
+private const val PrefetchTimeoutMillis = 4000L
 
 @AndroidEntryPoint
 class PrefetchActivity : ComponentActivity() {
@@ -32,20 +52,84 @@ class PrefetchActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      LaunchedEffect(Unit) {
-        applicationContext.dataStore.removeCovidCenterData()
+      MaterialTheme {
+        val progressPercentAnimatable = remember {
+          Animatable(
+            initialValue = 0f,
+            typeConverter = Float.VectorConverter,
+            label = "PrefetchProgress",
+          )
+        }
 
-        val jobs = ArrayList<Job>(10)
-        val items = ArrayList<CovidCenterItem>(100)
-        repeat(10) { pageIndex ->
-          jobs += launch(Dispatchers.IO) {
-            items += vm.getCenterList(page = pageIndex)
+        LaunchedEffect(Unit) {
+          var covidCenterDataSaveComplete = false
+          var progressContinuation: CancellableContinuation<Unit>? = null
+
+          launch {
+            applicationContext.dataStore.removeCovidCenterData()
+
+            val jobs = ArrayList<Job>(10)
+            val items = ArrayList<CovidCenterItem>(100)
+            repeat(10) { pageIndex ->
+              jobs += launch(Dispatchers.IO) {
+                items += vm.getCenterList(page = pageIndex)
+              }
+            }
+
+            jobs.joinAll()
+            applicationContext.dataStore.writeCovidCenterData(items)
+            covidCenterDataSaveComplete = true
+            progressContinuation?.resume(Unit)
+          }
+
+          // 1. 2초에 걸쳐 100%
+          // 2. API 데이터 저장이 완료되지 않았다면 80%에서 대기
+          // 3. 저장이 완료되면 0.4초에 걸쳐 100%
+          launch {
+            progressPercentAnimatable.animateTo(
+              targetValue = 0.8f,
+              animationSpec = tween(durationMillis = 1600, easing = LinearEasing),
+            )
+
+            suspend fun toFinishProgress() {
+              // LinearEasing이라 velocity 복원 불필요
+              progressPercentAnimatable.animateTo(
+                targetValue = 1f,
+                animationSpec = tween(durationMillis = 400, easing = LinearEasing),
+              )
+              changeActivityWithAnimation<MapActivity>()
+            }
+
+            if (covidCenterDataSaveComplete) {
+              toFinishProgress()
+            } else {
+              withTimeout(PrefetchTimeoutMillis) {
+                @Suppress("RemoveExplicitTypeArguments")
+                suspendCancellableCoroutine<Unit> { continuation ->
+                  progressContinuation = continuation
+                }
+              }
+              toFinishProgress()
+            }
           }
         }
 
-        jobs.joinAll()
-        applicationContext.dataStore.writeCovidCenterData(items)
+        Box(
+          modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.background),
+          contentAlignment = Alignment.Center,
+        ) {
+          LinearProgressIndicator(progress = progressPercentAnimatable.value)
+        }
       }
     }
   }
+}
+
+private inline fun <reified T : Activity> Activity.changeActivityWithAnimation(
+  intentBuilder: Intent.() -> Intent = { this },
+) {
+  startActivity(intentBuilder(Intent(this, T::class.java)))
+  overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
 }
