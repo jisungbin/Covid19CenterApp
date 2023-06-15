be56a067610ba33f983df25213ef05a7faca598b
jisungbin <ji@sungb.in>
2023-06-15 22:36:28 +0900

    PrefetchActivity에 데이터 prefetch 작업 추가

--- app/src/main/kotlin/io/github/jisungbin/covid19center/activity/PrefetchActivity.kt
+++ app/src/main/kotlin/io/github/jisungbin/covid19center/activity/PrefetchActivity.kt
@@ -10,13 +10,42 @@ package io.github.jisungbin.covid19center.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import dagger.hilt.android.AndroidEntryPoint
+import io.github.jisungbin.covid19center.datasource.dataStore
+import io.github.jisungbin.covid19center.datasource.removeCovidCenterData
+import io.github.jisungbin.covid19center.datasource.writeCovidCenterData
+import io.github.jisungbin.covid19center.model.domain.CovidCenterItem
+import io.github.jisungbin.covid19center.viewmodel.CovidCenterViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class PrefetchActivity : ComponentActivity() {
+
+  @Inject
+  lateinit var vm: CovidCenterViewModel
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Text("Hello, World!")
+      LaunchedEffect(Unit) {
+        applicationContext.dataStore.removeCovidCenterData()
+
+        val jobs = ArrayList<Job>(10)
+        val items = ArrayList<CovidCenterItem>(100)
+        repeat(10) { pageIndex ->
+          jobs += launch(Dispatchers.IO) {
+            items += vm.getCenterList(page = pageIndex)
+          }
+        }
+
+        jobs.joinAll()
+        applicationContext.dataStore.writeCovidCenterData(items)
+      }
     }
   }
 }
