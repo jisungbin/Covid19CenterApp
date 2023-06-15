e3383f66ca0d3fd14e6b8aa3a04ede862d202524
jisungbin <ji@sungb.in>
2023-06-15 18:03:27 +0900

    인터넷 권한 추가 및 힐트 초기화

---
+++ app/src/main/kotlin/io/github/jisungbin/covid19center/App.kt
@@ -0,0 +1,14 @@
+/*
+ * Designed and developed by Ji Sungbin 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/jisungbin/Covid19CenterApp/blob/main/LICENSE
+ */
+
+package io.github.jisungbin.covid19center
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
