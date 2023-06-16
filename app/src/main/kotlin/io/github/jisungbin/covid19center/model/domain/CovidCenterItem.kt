45c13fb8d65a714d10687999113e702e3ba3bd1e
jisungbin <ji@sungb.in>
2023-06-16 22:35:43 +0900

    코로나 센터 타입에 따른 마커 색상 계산 로직 추가

--- app/src/main/kotlin/io/github/jisungbin/covid19center/model/domain/CovidCenterItem.kt
+++ app/src/main/kotlin/io/github/jisungbin/covid19center/model/domain/CovidCenterItem.kt
@@ -7,8 +7,11 @@
 
 package io.github.jisungbin.covid19center.model.domain
 
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.graphics.Color
 import io.github.jisungbin.covid19center.model.SimpleLatLng
 import java.util.Date
+import kotlin.random.Random
 
 data class CovidCenterItem(
   val id: Int,
@@ -20,3 +23,19 @@ data class CovidCenterItem(
   val phoneNumber: String,
   val updatedAt: Date,
 )
+
+private val Color.Companion.Random
+  get(): Color {
+    val random = Random(System.currentTimeMillis())
+
+    val red = random.nextInt(256 / 2)
+    val green = random.nextInt(256 / 2)
+    val blue = random.nextInt(256 / 2)
+
+    return Color(red = red, green = green, blue = blue)
+  }
+private val MarkerColors = mutableMapOf<String, Color>()
+
+@Stable
+fun markerColorForCenterType(centerType: String) =
+  MarkerColors.getOrPut(centerType) { Color.Random }
