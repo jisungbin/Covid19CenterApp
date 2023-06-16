547d71e768627091178fba4491659541739faf3f
jisungbin <ji@sungb.in>
2023-06-17 00:17:42 +0900

    역직렬화에 사용할 Locale 한국으로 설정

--- app/src/main/kotlin/io/github/jisungbin/covid19center/datasource/mapper/object.kt
+++ app/src/main/kotlin/io/github/jisungbin/covid19center/datasource/mapper/object.kt
@@ -10,6 +10,7 @@ package io.github.jisungbin.covid19center.datasource.mapper
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.util.Locale
 import java.util.TimeZone
 
 val objectMapper =
@@ -18,4 +19,5 @@ val objectMapper =
     .disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
     .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
     .setTimeZone(TimeZone.getTimeZone("Asia/Seoul"))
+    .setLocale(Locale.KOREA)
     ?: error("ObjectMapper 생성에 실패했습니다.")
