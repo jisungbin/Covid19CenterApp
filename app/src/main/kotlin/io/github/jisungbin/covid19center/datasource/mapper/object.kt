af90164a9d31fbaab31917f04f7dbff750781c1c
Ji Sungbin <ji@sungb.in>
2023-06-17 01:08:51 +0900

    Fix timezone & locate (#15)
    * Date 역직렬화에 사용할 타임존 한국으로 설정
    
    * 역직렬화에 사용할 Locale 한국으로 설정

--- app/src/main/kotlin/io/github/jisungbin/covid19center/datasource/mapper/object.kt
+++ app/src/main/kotlin/io/github/jisungbin/covid19center/datasource/mapper/object.kt
@@ -10,9 +10,14 @@ package io.github.jisungbin.covid19center.datasource.mapper
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.util.Locale
+import java.util.TimeZone
 
-val objectMapper: ObjectMapper =
+val objectMapper =
   ObjectMapper()
     .registerKotlinModule()
     .disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
     .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+    .setTimeZone(TimeZone.getTimeZone("Asia/Seoul"))
+    .setLocale(Locale.KOREA)
+    ?: error("ObjectMapper 생성에 실패했습니다.")
